96cd4b07056fc61a98e8a9a366c71a51ec709e34
anu8942 <anuinfotech2001@gmail.com>
2023-09-16 12:10:34 +0530

    app file pushed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function App() {
+//   const [count , setcount] =
+//  usestate(0)
+//  console.log(count)
+  return (
+    <div>
+      
+    <button>Button</button>
+    </div>
+  )
+}
+
+
